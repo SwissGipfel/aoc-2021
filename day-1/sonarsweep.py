72aaeaa1af45f9320c9c570fd93404ffbdc4dc9c
SwissGipfel <SwissGipfel@gmx.ch>
2021-12-02 13:58:38 +0100

    solution day-1-B

--- day-1/sonarsweep.py
+++ day-1/sonarsweep.py
@@ -4,9 +4,21 @@ textFile.close()
 
 measurPoints = [int(line) for line in lines]
 
-dephtIncrease=0
-for index, value in enumerate(measurPoints):
-    if index > 0 and measurPoints[index-1] < value:
-        dephtIncrease+=1
+def getDephtIncrease(entries):
+    dephtIncrease=0
+    for index, value in enumerate(entries):
+        if index > 0 and entries[index-1] < value:
+            dephtIncrease+=1
+    return dephtIncrease
 
-print(dephtIncrease)
+def consolidateMeasure(entries, chunkSize, overlap):
+    result = []
+    for index in range(0, len(entries), chunkSize-overlap):
+        result.append(sum(entries[index:index + chunkSize]))
+    return result
+
+print("Depthincrease single measures:", getDephtIncrease(measurPoints))
+
+flattendMeasurePoints=consolidateMeasure(measurPoints, 3, 2)
+
+print("Flattend points dephincrease:", getDephtIncrease(flattendMeasurePoints))
